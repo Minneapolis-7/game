ea82987d7d41880e659bae3055ea8cb64cd25834
andreyvolokitin <andreyvlktn@gmail.com>
2021-08-06 22:27:10 +0400

    fix: убрать закомментированный код

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,6 @@ module.exports = {
         'plugin:import/typescript',
         'plugin:prettier/recommended',
       ],
-      // parser: '@typescript-eslint/parser',
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
@@ -75,19 +74,6 @@ module.exports = {
         react: {
           version: 'detect',
         },
-        // propWrapperFunctions: [
-        //   // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-        //   'forbidExtraProps',
-        //   { property: 'freeze', object: 'Object' },
-        //   { property: 'myFavoriteWrapper' },
-        // ],
-        // componentWrapperFunctions: [
-        //   // The name of any function used to wrap components, e.g. Mobx `observer` function. If this isn't set, components wrapped by these functions will be skipped.
-        //   'observer', // `property`
-        //   { property: 'styled' }, // `object` is optional
-        //   { property: 'observer', object: 'Mobx' },
-        //   { property: 'observer', object: '<pragma>' }, // sets `object` to whatever value `settings.react.pragma` is set to
-        // ],
         linkComponents: [
           // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
           'Hyperlink',
