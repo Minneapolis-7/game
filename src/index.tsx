54b92fcbd0fc97357a0c92b833936d0aa2fd2b47
andreyvolokitin <andreyvlktn@gmail.com>
2021-08-10 21:17:37 +0400

    task-2: fix: добавить размеры полям форм

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import './css/main.scss';
 import { Button, ButtonLink, Icon, Input, Textarea } from 'components';
 
 ReactDOM.render(
-  <>
+  <div style={{ padding: '20px' }}>
     <Button icon={<Icon name={icon.id} />} theme="2" />
     <Button sizing="lg" icon={<Icon name={icon.id} />} theme="2">
       Кнопка
@@ -18,8 +18,11 @@ ReactDOM.render(
     </ButtonLink>
     <ButtonLink href="//google.com">Ссылка</ButtonLink>
     <Icon size="2rem" name={icon.id} />
+    <Input sizing="xl" className="gap-y-lg" hint="Инпут" />
+    <Input sizing="lg" className="gap-y-lg" hint="Инпут" />
     <Input className="gap-y-lg" hint="Инпут" />
+    <Input sizing="sm" className="gap-y-lg" hint="Инпут" />
     <Textarea display="inline" cols={50} rows={10} hint="Test" />
-  </>,
+  </div>,
   document.getElementById('root')
 );
