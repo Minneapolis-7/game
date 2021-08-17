275ecbcce2b37a299a2a8e70faa282feb482d0bc
Daria Zherebtsova <dazhe2309@yandex.ru>
2021-08-17 22:10:07 +0300

    task-20: feat: добавить цвета

--- src/pages/LeaderboardPage/LeaderboardPage.tsx
+++ src/pages/LeaderboardPage/LeaderboardPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { block } from 'bem-cn';
 
 import Page from 'layout/Page';
-// import Profile from 'modules/Profile';
 
 const b = block('leaderboard');
 
@@ -23,28 +22,37 @@ const mockUserList = [
     points: 86,
   },
   {
+    id: 1,
+    nickname: 'Alonso',
+    points: 80,
+  },
+  {
     id: 15,
     nickname: 'Hamilton',
     points: 72,
+  },
+  {
+    id: 4,
+    nickname: 'Lauda',
+    points: 68,
   },
 ];
 
 type TableRowProps = {
   label: string;
-  value?: number;
+  value: number;
+  color: number;
 };
 
 function LeaderboardPage({ title }: { title: string }): JSX.Element {
-  function TableRow({ label, value }: TableRowProps): JSX.Element {
-    // ZERO-WIDTH-SPACE (\u200B) — на случай, если `value` пуст, нужно для выравнвиания текста
-
+  function TableRow({ label, value, color }: TableRowProps): JSX.Element {
     return (
       <tr className={b('table-row')}>
         <th className={b('table-cell')}>
-          <span className={b('field')}>{label}</span>
+          <span className={b('field').mix(`color-${color}`)}>{label}</span>
         </th>
         <td className={b('table-cell')}>
-          <span className={b('field')}>{value || '\u200B'}</span>
+          <span className={b('field').mix(`color-${color}`)}>{value || '\u200B'}</span>
         </td>
       </tr>
     );
@@ -59,8 +67,10 @@ function LeaderboardPage({ title }: { title: string }): JSX.Element {
         <div className={b('content')}>
           <table className={b('table')}>
             <tbody className={b('table-body')}>
-              {mockUserList.map((user) => {
-                return <TableRow key={user.id} label={user.nickname} value={user.points} />;
+              {mockUserList.map((user, index) => {
+                return (
+                  <TableRow key={user.id} label={user.nickname} value={user.points} color={index} />
+                );
               })}
             </tbody>
           </table>
