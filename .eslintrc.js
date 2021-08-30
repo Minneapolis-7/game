b558aca47f78d5a689410b1aaeb6ca312194cd3b
andreyvolokitin <andreyvlktn@gmail.com>
2021-08-30 15:15:52 +0400

    task-64: feat: линтить отступы у блоков/return/переменных

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,16 @@ module.exports = {
         'newlines-between': 'always',
       },
     ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'block' },
+      { blankLine: 'always', prev: 'block', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'block-like' },
+      { blankLine: 'always', prev: 'block-like', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+    ],
   },
 
   // Линтинг TypeScript (сорсы и сервер)
