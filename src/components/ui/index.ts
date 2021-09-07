d5c1618aff2676063fa48eb6be8956c9a863a084
andreyvolokitin <andreyvlktn@gmail.com>
2021-09-07 15:17:10 +0400

    task-90: fix: реэкспоритровать спиннер

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,3 +6,4 @@ export { default as Icon } from './Icon';
 export { default as Avatar } from './Avatar';
 export { default as Filepick } from './Filepick';
 export { default as Pagination } from './Pagination';
+export { default as Spinner } from './Spinner';
