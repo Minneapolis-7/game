7bf98771c8cc5429b10eda64651a8586df479a8b
andreyvolokitin <andreyvlktn@gmail.com>
2021-09-21 13:22:43 +0400

    task-99: docs: добавить докуметацию для useProgress

--- src/shared/utils/hooks/useProgress.ts
+++ src/shared/utils/hooks/useProgress.ts
@@ -1,5 +1,12 @@
 import { useCallback, useState } from 'react';
 
+/**
+ * Переиспользование логики при запуске асинхронных действий.
+ * Возвращает саму функцию, обработанную через `useCallback`, и флаг состояния.
+ * @param action - асинхронная функция, которую нужно исполнить
+ * @param keepProgress - не сбрасывать флаг `isInProgress` после завершения функции
+ * @returns [isInProgress, cb] - флаг состояния, и функция для запуска
+ */
 export default function useProgress(
   action: () => Promise<unknown>,
   keepProgress = false
