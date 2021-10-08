cffb3b889a2c1e6f2fe7199defd6bc932ab61883
Sergey Mukhin <50169307+mukhindev@users.noreply.github.com>
2021-10-08 17:43:18 +0300

    task-101: реализовать fullscreen в Safari и Firefox
    * task-101: реализовать fullscreen в Safari и Firefox
    
    * task-101: внести правки после ревью

--- src/modules/GameScreen/GameScreen.tsx
+++ src/modules/GameScreen/GameScreen.tsx
@@ -1,14 +1,16 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { block } from 'bem-cn';
 
 import { GameReactComponent } from '@/components';
-import { Button } from '@/components/ui';
+import { Button, Icon } from '@/components/ui';
 import { GameState } from '@/game/types';
 import text from '@/shared/const/text';
 
 import introImage from 'assets/img/game/intro.png';
 import lossImage from 'assets/img/game/loss.png';
 import winImage from 'assets/img/game/win.png';
+import fullscreenSvg from 'bootstrap-icons/icons/fullscreen.svg';
+import fullscreenExitSvg from 'bootstrap-icons/icons/fullscreen-exit.svg';
 
 import './game-screen.scss';
 
@@ -24,6 +26,10 @@ const { game: txt } = text;
 
 function GameScreen(): JSX.Element {
   const [gameScreen, setGameScreen] = useState(GAME_SCREEN.START);
+  const [fullscreen, setFullscreen] = useState(false);
+  const [fullscreenSupport, setFullscreenSupport] = useState(false);
+
+  const gameScreenRef = useRef<HTMLDivElement>(null);
 
   const handleGameStart = useCallback(() => {
     setGameScreen(GAME_SCREEN.PLAY);
@@ -43,10 +49,63 @@ function GameScreen(): JSX.Element {
     }
   };
 
+  useEffect(() => {
+    if (document.fullscreenElement === null && document.exitFullscreen) {
+      setFullscreenSupport(true);
+    }
+
+    const listenFullscreen = () => {
+      if (!document.fullscreenElement) {
+        setFullscreen(false);
+      }
+    };
+
+    document.addEventListener('fullscreenchange', listenFullscreen);
+
+    return () => {
+      document.removeEventListener('fullscreenchange', listenFullscreen);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!fullscreenSupport || !gameScreenRef.current) {
+      return;
+    }
+
+    if (fullscreen) {
+      gameScreenRef.current.requestFullscreen();
+    }
+
+    if (!fullscreen && document.fullscreenElement) {
+      document.exitFullscreen();
+    }
+  }, [fullscreenSupport, fullscreen]);
+
+  const handleToggleFullscreen = useCallback(() => {
+    setFullscreen((prevValue) => !prevValue);
+  }, [setFullscreen]);
+
+  const fullscreenButtonElement = useMemo(() => {
+    if (!fullscreenSupport) {
+      return null;
+    }
+
+    return (
+      <Button
+        className={b('fullscreen-button')}
+        theme="morphed"
+        sizing="lg"
+        icon={<Icon name={fullscreen ? fullscreenExitSvg.id : fullscreenSvg.id} scale={1.15} />}
+        onClick={handleToggleFullscreen}
+      />
+    );
+  }, [fullscreenSupport, fullscreen, handleToggleFullscreen]);
+
   const gameScreenElement = useMemo(() => {
     return {
       [GAME_SCREEN.START]: (
-        <div className={b({ [GAME_SCREEN.START]: true })}>
+        <div className={b({ [GAME_SCREEN.START]: true, fullscreen })} ref={gameScreenRef}>
+          {fullscreenButtonElement}
           <img className="liquid-img" src={introImage} alt="Персонаж игры в прыжке за ключом" />
           <Button onClick={handleGameStart} sizing="lg">
             {txt.playButton}
@@ -54,12 +113,14 @@ function GameScreen(): JSX.Element {
         </div>
       ),
       [GAME_SCREEN.PLAY]: (
-        <div className={b({ [GAME_SCREEN.PLAY]: true })}>
-          <GameReactComponent onStateUpdate={handleStateUpdate} />
+        <div className={b({ [GAME_SCREEN.PLAY]: true, fullscreen })} ref={gameScreenRef}>
+          {fullscreenButtonElement}
+          <GameReactComponent onStateUpdate={handleStateUpdate} fullscreen={fullscreen} />
         </div>
       ),
       [GAME_SCREEN.WIN]: (
-        <div className={b({ [GAME_SCREEN.WIN]: true })}>
+        <div className={b({ [GAME_SCREEN.WIN]: true, fullscreen })} ref={gameScreenRef}>
+          {fullscreenButtonElement}
           <img className="liquid-img" src={winImage} alt="Персонаж игры выходит в открытые двери" />
           <h2 className="heading_2 heading">{txt.winText}</h2>
           <Button onClick={handleGameStart} sizing="lg">
@@ -68,7 +129,8 @@ function GameScreen(): JSX.Element {
         </div>
       ),
       [GAME_SCREEN.LOSS]: (
-        <div className={b({ [GAME_SCREEN.LOSS]: true })}>
+        <div className={b({ [GAME_SCREEN.LOSS]: true, fullscreen })} ref={gameScreenRef}>
+          {fullscreenButtonElement}
           <img className="liquid-img" src={lossImage} alt="Персонаж игры лежит после проигрыша" />
           <h2 className="heading_2 heading">{txt.lossText}</h2>
           <Button onClick={handleGameStart} sizing="lg">
@@ -77,7 +139,7 @@ function GameScreen(): JSX.Element {
         </div>
       ),
     };
-  }, [handleGameStart]);
+  }, [handleGameStart, fullscreenButtonElement, fullscreen]);
 
   return gameScreenElement[gameScreen];
 }
