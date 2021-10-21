e246243af2870ff245fb0b2a7b0971df1567fddc
andreyvolokitin <andreyvlktn@gmail.com>
2021-10-21 15:09:13 +0400

    task-150: feat: начать таск

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ COPY --from=build /app/node_modules /node_modules
 COPY --from=build /app/start-server.js /start-server.js
 COPY --from=build /app/.env /.env
 # todo: `npm i dotenv` после того, как `node_modules` будут удалены: https://github.com/Minneapolis-7/game/issues/111
-CMD POSTGRES_DB_HOST=postgres node -r dotenv/config start-server.js
+CMD POSTGRES_DB_HOST=postgres npx sequelize-cli db:create && node -r dotenv/config start-server.js
--- start-server.js
+++ start-server.js
@@ -1,11 +1,15 @@
 // eslint-disable-next-line import/no-unresolved, import/extensions
 const { app, sequelize } = require('./dist/server.js');
 
+const { execSync } = require('child_process');
+
 const { APP_PORT = 4000 } = process.env;
 
 (async () => {
   try {
     await sequelize.sync({ alter: process.env.NODE_ENV !== 'production' });
+
+    execSync('npm run db:seed');
 
     app.listen(APP_PORT, () => console.log(`Server http://localhost:${APP_PORT}`));
   } catch (e) {
