db167fcc92dd2d8ae80ccd38514945421fcdde76
andreyvolokitin <andreyvlktn@gmail.com>
2021-10-23 13:25:18 +0400

    task-150: fix: рефакторить

--- start-server.js
+++ start-server.js
@@ -4,9 +4,9 @@ const { app, sequelize } = require('./dist/server.js');
 const { execSync } = require('child_process');
 const fs = require('fs');
 
+const DB_SEEDED_CHECK_PATH = './db/is_seeded';
 const { APP_PORT = 4000 } = process.env;
 const isProduction = process.env.NODE_ENV === 'production';
-const DB_SEEDED_CHECK_PATH = './db/is_seeded';
 const isDbSeeded = fs.existsSync(DB_SEEDED_CHECK_PATH);
 
 (async () => {
