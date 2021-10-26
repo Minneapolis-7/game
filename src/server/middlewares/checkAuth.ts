a5d00f51cc8e7e54d9607c3f5c6f2dd5ad2801b6
andreyvolokitin <andreyvlktn@gmail.com>
2021-10-26 20:08:35 +0400

    task-144: fix фиксить баг, поменять OAuth redirect URI

--- src/server/middlewares/checkAuth.ts
+++ src/server/middlewares/checkAuth.ts
@@ -5,10 +5,10 @@ import userApi from '@/api/userApi';
 export default async function checkAuth(req: Request, res: Response, next: NextFunction) {
   try {
     res.locals.user = await userApi.getUser(req.cookies.authCookie);
+
+    next();
   } catch (e) {
     console.log('checkAuth error: ', e);
-    next();
-  } finally {
     next();
   }
 }
--- src/shared/const/const.ts
+++ src/shared/const/const.ts
@@ -4,7 +4,7 @@ export const API_CUSTOM = '/api/v1';
 
 export const YANDEX_OAUTH = 'https://oauth.yandex.ru/authorize';
 
-export const OAUTH_REDIRECT_URI = 'https://minneapolis-game.herokuapp.com/';
+export const OAUTH_REDIRECT_URI = 'https://minneapolis-game-7.ya-praktikum.tech';
 
 export const RETINA_SUFFIX = '@2x';
 
