2a98761c22bc0f4e2f84390841ae73390759daf5
andreyvolokitin <andreyvlktn@gmail.com>
2021-10-26 22:29:09 +0400

    task-109: fix: рефакторить

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -3,6 +3,7 @@ import type { Optional } from 'utility-types';
 import { SiteThemeAttributes } from '@/server/sequelize/models/Themes/SiteTheme';
 import { UserCreationAttributes } from '@/server/sequelize/models/User';
 import { UserUpdatePayload } from '@/server/services/userService';
+import { DEFAULT_USER_NICKNAME } from '@/shared/const/const';
 
 import { apiCustom, apiYandex } from './api';
 import {
@@ -39,7 +40,7 @@ export default {
 
     await this.updateProfile({
       ...userCopy,
-      displayName: `player-${data.id}`,
+      displayName: `${DEFAULT_USER_NICKNAME}-${data.id}`,
     });
 
     return data.id;
