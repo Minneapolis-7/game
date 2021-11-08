b1f543d0d206eab05c5a1417089e70e6dcc1b9cb
andreyvolokitin <created42@gmail.com>
2021-11-08 18:10:01 +0400

    task-176: fix: позволить локальную разработку
    защищать ручки и делать protected-редиректы на сервере только в продакшене, при разработке клиент будет иметь возможность сам получить юзера

--- src/server/middlewares/protect.ts
+++ src/server/middlewares/protect.ts
@@ -2,10 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 
 import { HttpStatuses } from '@/shared/const/const';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default async function protect(req: Request, res: Response, next: NextFunction) {
   const { yandexUser } = req.app.locals;
 
-  if (!yandexUser) {
+  if (isProduction && !yandexUser) {
     res.sendStatus(HttpStatuses.CLIENT_ERROR_UNAUTHORIZED);
 
     return;
